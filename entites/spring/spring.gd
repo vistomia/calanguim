412b5a87160bd81693b57b055221bbed49db2efe
vistomia <victor.farias.0108@gmail.com>
2025-05-31 16:04:39 -0300

    feat: fix spring collision

--- entites/spring/spring.gd
+++ entites/spring/spring.gd
@@ -1,6 +1,12 @@
 extends StaticBody2D
 
+var is_extend = false
 
 func _on_area_jump_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
+	if body.name == "Player" and !is_extend:
 		body.spring_jump()
+		is_extend = true
+		$Reload.start()
+
+func _on_reload_timeout() -> void:
+	is_extend = false
